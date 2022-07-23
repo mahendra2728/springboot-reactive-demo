5880169700e0e7f988494c03b04311bdb771c0dd
Mahendra M <mahendramaske28@gmail.com>
2022-07-23 17:12:24 +0530

    first commit

---
+++ src/test/java/com/pm/reactive/demo/SpringbootReactiveDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.reactive.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootReactiveDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
